060384338c9c8a26fb4247b669cffba5047a7910
Shu-Ling Fang <selina24228@gmail.com>
2020-10-06 10:49:26 +0800

    finish baseline

--- main.js
+++ main.js
@@ -5,11 +5,44 @@ var button = document.getElementById("cal-button")
 input.value = ''
 color.value = '#b0b0b0'
 
+var last_select = -1;
+
+//Add EventListener to cells, clicks->background-color=black.
+for(var i=0; i<cells.length; ++i) {
+    cells[i].addEventListener('click', function(){
+		this.style.backgroundColor = "#000000";
+		this.style.color = "white";
+		if(last_select > 0){
+			cells[last_select-4].style.backgroundColor="white";
+			cells[last_select-4].style.color="black";
+		}
+		last_select = this.innerHTML.split('\n')[0];
+		console.log(last_select);
+	})
+}
+
+input.addEventListener('keypress', function(e){
+	if(e.code =='Enter' && input.value != ''){
+		if(last_select < 0){
+			alert("Please select one date first!!!");
+		}
+		else{
+			cells[last_select-4].innerHTML = last_select+'\n'+input.value;
+			input.value = '';
+		}
+	}
+})
 
 
-
-
-
+button.addEventListener('click', function(){
+	if(last_select < 0){
+			alert("Please select one date first!!!");
+	}
+	else{
+		cells[last_select-4].innerHTML = last_select+'\n'+input.value;
+		input.value = '';
+	}
+})
 
 
 
